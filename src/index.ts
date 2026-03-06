bc0bbdcfc86c1ce652b81901cf6f18edbd37ae6a
ao-oj05 <aliciaocana49@gmail.com>
2026-03-06 07:52:09 -0600

    Cambios

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,9 @@ app.use("/api", playersRoutes);
 
 const PORT = process.env.PORT || 4000;
 
+app.get("/", (req, res) => {
+    res.send("Bienvenido a la API del FC Barcelona. Los datos están en /api/players");
+});
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
   console.log(`API disponible en: http://localhost:${PORT}/api/players`);
